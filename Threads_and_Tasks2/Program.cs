541a49ed76a959afe7a11f5e7636a7c4b31f0f7d
Milena Yeghoyan <minela.yeghoyan1@gmail.com>
2023-07-17 14:42:19 +0400

    Add project files.

---
+++ Threads_and_Tasks2/Program.cs
@@ -0,0 +1,57 @@
+﻿
+//Task1 - 
+using System.Diagnostics;
+
+try
+{
+    string file1 = "C:\\Users\\user\\Desktop\\File1.txt";
+    string file2 = "C:\\Users\\user\\Desktop\\File2.txt";
+
+    var task1 = Task.Run(() => CountFileLines(file1, 1));
+    var task2 = Task.Run(() => CountFileLines(file2, 2));
+
+    int fileOneLinesCount = task1.Result;
+    int fileTwoLinesCount = task2.Result;
+
+    int totalLines = fileOneLinesCount + fileTwoLinesCount;
+    Console.WriteLine();
+    Console.Write($"Total line counts of two files is :{totalLines}");
+
+    Console.Read();
+}
+catch (FileNotFoundException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+catch (IOException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+catch (Exception ex)
+{
+    Console.WriteLine($"General error. Exception message is:{ex.Message}");
+}
+
+int CountFileLines(string filePath, int fileNumber)
+{
+    using (TextReader reader = new StreamReader(filePath))
+    {
+        int count = 0;
+        string line = string.Empty;
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        while ((line = reader.ReadLine()) != null)
+        {
+            count++;
+            Thread.Sleep(1000);
+            Console.WriteLine($"For file: {fileNumber}. Elapsed time is:{stopwatch.ElapsedMilliseconds}");
+        }
+
+        stopwatch.Stop();
+
+        return count;
+    }
+}
